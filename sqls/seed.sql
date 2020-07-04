18e14f1a94e9c095b9bccc4752ac97f5b5ac0deb
affluent-bilby-classifieds <40859915+affluent-bilby-classifieds@users.noreply.github.com>
2020-07-04 15:34:00 +0800

    Update seed.sql

--- sqls/seed.sql
+++ sqls/seed.sql
@@ -39,3 +39,19 @@ CREATE TABLE public.menucategories (
     catname        VARCHAR(255) NOT NULL,
     created_at     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE items (
+  id BIGSERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  email TEXT NOT NULL,
+  hash_pwd TEXT NOT NULL,
+  token TEXT NOT NULL
+);
+
+INSERT INTO items VALUES(Default, 'item1', 'description1');
+INSERT INTO items VALUES(Default, 'item2', 'description2');
